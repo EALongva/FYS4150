f66b8648a410e05fda7246936c00296cfefcd783
EALongva <erlenalo@student.ifikk.uio.no>
2020-09-30 12:07:14 +0200

    added labels and misc to the buckling beam plots

--- project2/scripts/bucklingbeam2.py
+++ project2/scripts/bucklingbeam2.py
@@ -5,13 +5,21 @@ import matplotlib.pyplot as plt
 
 X = np.genfromtxt("dat/eigenvalues.csv", delimiter=',',skip_header=1)
 x = np.linspace(1, X.shape[0], X.shape[0]) # generating the x-arrays
+rho = np.linspace(0,1,X.shape[0])
 
 plt.figure()
 
 plt.subplot(211)
 plt.plot(x, X[:,0], 'g', x, X[:,1], 'r--')
+plt.title("Buckling beam, eigenvalues, rhoN=1")
+plt.xlabel("i")
+plt.ylabel("eigenvalue i")
+
 
 plt.subplot(212)
-plt.plot(x, X[:,2], 'g', x, X[:,3], 'r--')
+plt.plot(rho, X[:,2], 'g', rho, X[:,3], 'r--')
+plt.title("Eigenvector of smallest eigenvalue")
+plt.xlabel("rho [m/L]")
+plt.ylabel("u(rho)")
 
 plt.show()

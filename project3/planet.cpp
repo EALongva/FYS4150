feec980febcedbc069e67f1f3e22b332d6ae30da
astridbg <astridbg@student.matnat.uio.no>
2020-10-06 14:51:15 +0200

    first-try planet class finished

---
+++ project3/planet.cpp
@@ -0,0 +1,49 @@
+#include "planet.hpp"
+
+using namespace arma;
+using namespace std;
+
+planet::planet(string name, double M, vec r, vec v)
+{
+    name = name;
+    mass = M;
+    position = r;
+    velocity = v;
+    kinetic = 0.0;
+    potential = 0.0;
+    force = 0.0;
+}
+
+vec planet::distance(const planet& otherPlanet)
+{
+  return position - otherPlanet.position;
+}
+
+vec planet::gravitationalForce(const planet& otherPlanet, double Gconst)
+{
+  vec r = distance(otherPlanet);
+  if (norm(r)!=0){
+    return Gconst * mass * otherPlanet.mass / pow(norm(r),3) * r;
+  }
+  else {return 0;}
+}
+
+vec planet::acceleration(const planet& otherPlanet, double Gconst)
+{
+  vec gForce = gravitationalForce(otherPlanet, Gconst);
+  return gForce / mass;
+}
+
+double planet::kineticEnergy()
+{
+  return 0.5 * mass * norm(velocity) * norm(velocity);
+}
+
+double planet::potentialEnergy(const planet& otherPlanet, double Gconst)
+{
+  vec r = distance(otherPlanet);
+  if(norm(r)!=0){
+    return -Gconst * mass * otherPlanet.mass / norm(r);
+  }
+  else {return 0;}
+}

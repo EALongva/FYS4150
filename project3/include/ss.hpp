422df751ee1e059e50e098e88916b38aeed2dc86
EALongva <erlenalo@student.ifikk.uio.no>
2020-10-13 21:47:49 +0200

    added euler and VVmethods

--- project3/include/ss.hpp
+++ project3/include/ss.hpp
@@ -20,6 +20,8 @@ class ss
     int total_planets;
     double G;
     std::vector<planet> all_planets;
+    std::vector<arma::mat> Xevo;
+    std::vector<arma::mat> Vevo;
 
 // constructor
   ss();
@@ -27,6 +29,11 @@ class ss
 // functions
   void add(planet newplanet);
   void planet_names();
+  void euler(double T, int N);
+  void eulerStep(int I, int J, int i, double dt);
+  void VVerlet(double T, int N);
+  void test(int nr);
+
 
 };
 

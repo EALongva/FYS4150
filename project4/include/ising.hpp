47b57dc6a1b105d912dbc9c1d102777ab9636a46
astridbg <astridbg@student.matnat.uio.no>
2020-11-06 16:55:06 +0100

    added variables in initialiser

--- project4/include/ising.hpp
+++ project4/include/ising.hpp
@@ -12,12 +12,15 @@ public:
   // Properties
   int N; // dimensions of lattice (LxL)
   double J; // coupling constant
-  double T; // temperature, same as energy with K_b=1
+  double T; // temperature, same as energy with k_b=1
   arma::mat state; // state spin configurations of system
+  arma::vec w;
+  int E;
+  int M;
 
   // Initializer
-  Ising(int N);
-  Ising(int N_in, double T_in, double J_in, int seed );
+  Ising(int N_in, double T_in);
+  Ising(int N_in, double T_in, double J_in);
 
   // Functions
   void genState();

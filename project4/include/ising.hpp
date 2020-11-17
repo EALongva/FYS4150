6f043bae6fbdeaff51bccf90c1fa76ca2bdc7d0b
EALongva <erlenalo@student.ifikk.uio.no>
2020-11-17 21:13:43 +0100

    created montecarlo function for ising class

--- project4/include/ising.hpp
+++ project4/include/ising.hpp
@@ -1,6 +1,7 @@
 #ifndef ISING_H
 #define ISING_H
 
+//#include "mpi.h"
 #include <iostream>
 #include <cmath>
 #include <armadillo>
@@ -12,25 +13,39 @@ public:
   // Properties
 
   int N; // dimensions of lattice (LxL)
+  int Nspins; // total spins
   int seed; // seeding our rngs
-  int E; // energy of model
-  int M; // magnetization
+  int ordered; // controls the initial matrix 0 = ordered, 1 = random
 
+  double E; // energy of model
+  double M; // magnetization
   double J; // coupling constant
   double T; // temperature, same as energy with K_b=1
+
+  std::mt19937_64 gen; // RNG object
+  std::uniform_real_distribution<double> URD; // uniform real dist [0,1]
 
   arma::mat state; // state spin configurations of system
   arma::vec w; // probability array (boltzmann<3)
 
+  arma::vec avgE;
+  arma::vec avgM;
+  arma::vec expE; // energy expectation value
+  arma::vec expM; // magnetisation expectation value
+
   // Initializer
   Ising(int N, double T_in);
-  Ising(int N_in, double T_in, double J_in, int seed_in );
+  Ising(int N_in, double T_in, double J_in, int seed_in, int ordered_in );
 
   // Functions
   void init();
   void genState();
   void energy();
   void Metropolis();
+  void MC(int cycles_in, int burnin);
+  void paraMC(int cycles_in);
+  void burnin(int cycles_in);
+  void save(std::string filename);
 
 };
 
--- project4/src/ising.cpp
+++ project4/src/ising.cpp
@@ -32,6 +32,7 @@ Ising::Ising(int N_in, double T_in)
 Ising::Ising(int N_in, double T_in, double J_in, int seed_in, int ordered_in)
 {
     N = N_in;
+    Nspins = N_in*N_in;
     cout << "init dimension = " << N << endl;
     E = 0;
     M = 0;
@@ -45,19 +46,19 @@ Ising::Ising(int N_in, double T_in, double J_in, int seed_in, int ordered_in)
       w(i) = - (1/T) * (i-8);
     }
     w = exp(w);
+
     seed = seed_in;
     ordered = ordered_in;
 
     // setting the arma::random seed
     arma::arma_rng::set_seed(seed);
 
-    // setting up the e^(-beta*dE) array, hence we dont compute this for
-    // every step
-    w = arma::vec(17, arma::fill::zeros);
-    for (int i=0; i < 17; i += 4){
-      w(i) = - (1/T) * (i-8);
-    }
-    w = arma::exp(w);
+    // initializing global RNG, define uniform real distribution x \in [[0, 1]
+    std::mt19937_64 gen(seed);
+    std::uniform_real_distribution<double> URD(0.0,1.0);
+
+    // initialize the state using the Ising::init() function
+    init();
 
 }
 
@@ -80,7 +81,7 @@ void Ising::init()
     state.print();
   }
 
-  int tempE = 0;
+  double tempE = 0;
   mat pS; // altered state having periodic boundaries (periodic State)
 
   // description in ising::energy()
@@ -93,13 +94,13 @@ void Ising::init()
   for (int i = 1; i <= N; i++){
     for (int j = 1; j <= N; j++){
 
-      tempE += -J*( pS(i,j)*pS(i-1,j) + pS(i,j)*pS(i+1,j)
-                    + pS(i,j)*pS(i,j-1) + pS(i,j)*pS(i,j+1) );
+      tempE += -J*pS(i,j)*( pS(i-1,j) + pS(i,j-1) );
 
     }
   }
 
   E = tempE;
+  M = arma::accu(state);
 
 }
 
@@ -121,7 +122,7 @@ void Ising::energy()
   mat pS; // altered state having periodic boundaries (periodic State)
 
   // concatenation of upper and lower rows to be looped over
-  // -> a somewhat tricky process, appends the last column to the
+  // -> a some what inefficient process, appends the last column to the
   //   start of the matrix, and the first original column to the
   //   end of the matrix, same procedure for rows
   //   this gives a matrix with periodic boundaries
@@ -151,40 +152,170 @@ void Ising::energy()
 
 void Ising::Metropolis()
 {
-  // Sweeing through the spins in the lattice and picking random spins
-  for (int y = 0; y < N; y++){
-    for (int x = 0; y < N; x++){
-      int ix = (int) rand() % N;
-      int iy = (int) rand() % N;
+  // Performs one metropolis alogorithm
 
-      // Computing the energy change dE
-      int dE = 2 * state(iy, ix) *
-        (state(iy, periodic(ix, N, -1)) +
-        state(periodic(iy, N, -1), ix) +
-        state(iy, periodic(ix, N, 1)) +
-        state(periodic(iy, N, 1), ix));
+  int ix = (int) (URD(gen)*N);
+  int iy = (int) (URD(gen)*N);
 
-      // We use our energy change dE to index a corresponding probability w,
-      // which is dependent on temperature. A dE=-8 corresponds to
-      // a probability w(dE+8)=w(0)=exp(8/(k_bT)).
-      // We flip the spin if the energy change is less than or equal to
-      // zero (which gives a w(dE+8)=>1) or if a generated random number between
-      // 0 and 1 is less than w(dE+8)
-      if (rand() / RAND_MAX <= w(dE+8)){
-        state(iy, ix) *= -1; // flip the spin
-        M += 2*state(iy, ix);
-        E += dE;
-      }
-    }
+  /*std::cout << "random indices: " << ix << iy << std::endl;
+  std::cout << "periodic neighbors: " << std::endl; */
+
+  // Computing the energy change dE
+  int dE = 2 * state(iy, ix) *
+    (state(iy, periodic(ix, N, -1)) +
+    state(periodic(iy, N, -1), ix) +
+    state(iy, periodic(ix, N, 1)) +
+    state(periodic(iy, N, 1), ix));
+
+  // We use our energy change dE to index a corresponding probability w,
+  // which is dependent on temperature. A dE=-8 corresponds to
+  // a probability w(dE+8)=w(0)=exp(8/(k_bT)).
+  // We flip the spin if the energy change is less than or equal to
+  // zero (which gives a w(dE+8)=>1) or if a generated random number between
+  // 0 and 1 is less than w(dE+8)
+
+  if (URD(gen) <= w(dE+8)){
+    state(iy, ix) *= -1; // flip the spin
+    M += 2*state(iy, ix);
+    E += dE;
+  }
+}
+
+void Ising::MC(int cycles_in, int burnin)
+{
+  if(burnin > 1 or burnin < 0){
+    std::cout << "invalid value for burnin, 0 = burnin false, 1 = burnin true" << std::endl;
   }
 
+  int cycles = std::pow(10, cycles_in);
+
+  arma::vec Energies(cycles, arma::fill::zeros);
+  arma::vec Magnetisations(cycles, arma::fill::zeros);
+  arma::vec AverageEnergies(cycles, arma::fill::zeros);
+  arma::vec AverageMagnetisations(cycles, arma::fill::zeros);
+
+  double SumAllEnergies = E/Nspins;
+  double SumAllMagnetisations = M/Nspins;
+
+  std::cout << SumAllEnergies << ", " << SumAllMagnetisations << std::endl;
+
+  for (int cyc = 0; cyc < cycles; cyc++){
+
+    for (int spin = 0; spin < Nspins; spin++){
+
+      Metropolis();
+
+    }
+
+    Energies(cyc) = E/Nspins;
+    Magnetisations(cyc) = fabs(M)/Nspins;
+    SumAllEnergies += Energies(cyc);
+    SumAllMagnetisations += Magnetisations(cyc);
+    AverageEnergies(cyc) = SumAllEnergies/(cyc+1);
+    AverageMagnetisations(cyc) = SumAllMagnetisations/(cyc+1);
+
+    std::cout << "sum: " << SumAllEnergies << ", " << SumAllMagnetisations << std::endl;
+    std::cout << "avg: " << AverageEnergies(cyc) << ", " << AverageMagnetisations(cyc) << std::endl;
+
+  }
+
+  avgE = AverageEnergies;
+  avgM = AverageMagnetisations;
 
 }
 
 
-void save(std::string filename)
+void Ising::paraMC(int cycles_in)
 {
-  
+
+  // make function that runs standard Monte Carlo in parallel, loop over time
+  // in other function
+
+  int cycles = std::pow(10, cycles_in);
+
+  arma::vec Energies(cycles, arma::fill::zeros);
+  arma::vec Magnetisations(cycles, arma::fill::zeros);
+  arma::vec AverageEnergies(cycles, arma::fill::zeros);
+  arma::vec AverageMagnetisations(cycles, arma::fill::zeros);
+
+  double SumAllEnergies = E/Nspins;
+  double SumAllMagnetisations = M/Nspins;
+
+  for (int cyc = 0; cyc < cycles; cyc++){
+
+    for (int spin = 0; spin < Nspins; spin++){
+
+      Metropolis();
+
+    }
+
+    Energies(cyc) = E/Nspins;
+    Magnetisations(cyc) = fabs(M)/Nspins;
+    SumAllEnergies += Energies(cyc);
+    SumAllMagnetisations += Magnetisations(cyc);
+    AverageEnergies(cyc) = SumAllEnergies/(cyc+1);
+    AverageMagnetisations(cyc) = SumAllMagnetisations/(cyc+1);
+
+  }
+
+  avgE = AverageEnergies;
+  avgM = AverageMagnetisations;
+
+}
+
+
+void Ising::burnin(int cycles_in)
+{
+
+  // cycles_in how many cycles to burn
+
+  int cycles = std::pow(10, cycles_in);
+
+  arma::vec Energies(cycles, arma::fill::zeros);
+  arma::vec Magnetisations(cycles, arma::fill::zeros);
+  arma::vec AverageEnergies(cycles, arma::fill::zeros);
+  arma::vec AverageMagnetisations(cycles, arma::fill::zeros);
+
+  double SumAllEnergies = E/Nspins;
+  double SumAllMagnetisations = M/Nspins;
+
+  for (int cyc = 0; cyc < cycles; cyc++){
+
+    for (int spin = 0; spin < Nspins; spin++){
+
+      Metropolis();
+
+    }
+
+    Energies(cyc) = E/Nspins;
+    Magnetisations(cyc) = fabs(M)/Nspins;
+    SumAllEnergies += Energies(cyc);
+    SumAllMagnetisations += Magnetisations(cyc);
+    AverageEnergies(cyc) = SumAllEnergies/(cyc+1);
+    AverageMagnetisations(cyc) = SumAllMagnetisations/(cyc+1);
+
+  }
+
+  avgE = AverageEnergies;
+  avgM = AverageMagnetisations;
+
+}
+
+
+void Ising::save(std::string filename)
+{
+  // saves average energies and magnetisations using the arma::save()
+
+  int length          = avgE.n_elem;
+  std::string len     = std::to_string(length);
+  std::string csv     = ".csv";
+
+  std::string fileout_avgE = "results/data/" + filename + "_avgE_" + len + csv;
+  std::string fileout_avgM = "results/data/" + filename + "_avgM_" + len + csv;
+
+  avgE.save(fileout_avgE, arma::csv_ascii);
+  avgM.save(fileout_avgM, arma::csv_ascii);
+
 }
 
 

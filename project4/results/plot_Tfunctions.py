22a865b9fe3864aa62b153c04903f50f54173d71
astridbg <astridbg@student.matnat.uio.no>
2020-11-18 15:48:11 +0100

    up to 80 size lattice

--- project4/results/plot_Tfunctions.py
+++ project4/results/plot_Tfunctions.py
@@ -2,14 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-lattice2=pd.read_csv("data/lattice2", index_col=False, sep="\s+",names=["T","E","Cv","M","Chi","|M|"])
-
-plt.figure()
-plt.plot(lattice2['T'], lattice2['Chi'])
-plt.show()
-
 lattice20=pd.read_csv("data/lattice20", index_col=False, sep="\s+",names=["T","E","Cv","M","Chi","|M|"])
+lattice40=pd.read_csv("data/lattice40", index_col=False, sep="\s+",names=["T","E","Cv","M","Chi","|M|"])
+lattice60=pd.read_csv("data/lattice60", index_col=False, sep="\s+",names=["T","E","Cv","M","Chi","|M|"])
+lattice80=pd.read_csv("data/lattice80", index_col=False, sep="\s+",names=["T","E","Cv","M","Chi","|M|"])
 
 plt.figure()
-plt.plot(lattice20['T'], lattice20['Chi'])
+plt.scatter(lattice20['T'], lattice20['Chi']/(20*20), label=r'$L=20$')
+plt.scatter(lattice40['T'], lattice40['Chi']/(40*40), label=r'$L=40$')
+plt.scatter(lattice60['T'], lattice60['Chi']/(60*60), label=r'$L=60$')
+plt.scatter(lattice80['T'], lattice80['Chi']/(80*80), label=r'$L=80$')
+plt.xlabel(r'Temperature [$k_BT/J$]')
+plt.ylabel(r'Susceptibility $\chi$')
+plt.grid()
+plt.legend(loc="best")
+plt.savefig("figures/susceptibility.png",bbox_inches = 'tight')
 plt.show()

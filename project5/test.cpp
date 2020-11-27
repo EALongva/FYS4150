5fb1567beb91ec86bcb18c3686e19cf684c64bfe
EALongva <erlenalo@student.ifikk.uio.no>
2020-11-27 16:09:46 +0100

    test function for p5

---
+++ project5/test.cpp
@@ -0,0 +1,24 @@
+// test file project 5
+
+#include <iostream>
+#include <armadillo>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include "time.h"
+#include "include/rossby.hpp"
+
+int main(){
+
+  // testing rossby class
+  ross rossby(3);
+  ross.function();
+
+  // generating an armadillo array to test data saving and plotting
+  arma::vec r(10,arma::fill::randu);
+  r.save("results/data/example.csv", arma::csv_ascii);
+
+  // test the algorithms for calculating the laplacian from project 1
+
+  return 0;
+}
